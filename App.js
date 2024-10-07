836840b2495daa7377b1f58b554a11f13ec432e5
PhungCanhTuan <tuancanh252@gmail.com>
2024-10-07 09:37:27 +0700

    Tuan 08

---
+++ App.js
@@ -0,0 +1,28 @@
+//PhungCanhTuan
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+import Login from './screens/LoginScreen';
+import Register from './screens/RegisterScreen';
+import HomeScreen from './screens/HomeScreen';
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} options={{headerShown:false}}/>
+        <Stack.Screen name="Register" component={Register} options={{headerShown:false}}/>
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
